d073d9d5c089c83211e0364d24e8e23ed2ac360e
MrGg570 <143704858+MrGg570@users.noreply.github.com>
2026-05-23 00:56:06 +0200

    run only if file not exist

--- convert_models.bat
+++ convert_models.bat
@@ -1,4 +1,4 @@
-gltf2bam.exe .\assets\Shop.glb .\assets\Shop.bam
-gltf2bam.exe .\assets\Tony.glb .\assets\Tony_run.bam
-gltf2bam.exe .\assets\Bartender.glb .\assets\bartender.bam
-gltf2bam.exe .\assets\Fredi.glb .\assets\quest_guy.bam    
+if not exist .\assets\Shop.bam gltf2bam.exe .\assets\Shop.glb .\assets\Shop.bam
+if not exist .\assets\Tony_run.bam gltf2bam.exe .\assets\Tony.glb .\assets\Tony_run.bam
+if not exist .\assets\bartender.bam gltf2bam.exe .\assets\Bartender.glb .\assets\bartender.bam
+if not exist .\assets\quest_guy.bam gltf2bam.exe .\assets\Fredi.glb .\assets\quest_guy.bam    
